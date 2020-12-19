4b620326ad5a89fb307c059533202afa20153de4
Woodensponge <43834352+Woodensponge@users.noreply.github.com>
2020-12-19 10:42:54 -0500

    Add consistent timestep for physics
    WIll be called timestep in the code once I work on the Timer class. This is a really really good start for now though.

--- Hare/Main.cpp
+++ Hare/Main.cpp
@@ -6,12 +6,8 @@
 
 int main()
 {
-    Application* app = new Application("Hare", WINDOW_WIDTH, WINDOW_HEIGHT, 0, 60, false);
+    Application* app = new Application("Hare", WINDOW_WIDTH, WINDOW_HEIGHT, 0, 60, true);
     app->Init();
-
-    //Setup variables to calculate Delta Time for game logic
-    Uint64 lastPC;
-    Uint64 presentPC = SDL_GetPerformanceCounter();
 
     //Setup variables to calculate Delta Time for framerate
     Uint32 lastTicks = SDL_GetTicks();
@@ -21,22 +17,31 @@ int main()
 
     while (app->state != GameState::Closing)
     {
-        //Calculate Delta Time for game logic
-        lastPC = presentPC;
-        presentPC = SDL_GetPerformanceCounter();
-        deltaTime = (double)((double)(presentPC - lastPC) / SDL_GetPerformanceFrequency());
-        std::cout << deltaTime << std::endl;
-
         //Get ticks right now to calculate Delta Time for framerate
         lastTicks = SDL_GetTicks();
 
+        /*
+        * The game gets updated based on the cap of the framerate (the SDL_Delay system going on).
+        * HOWEVER, deltatime is always based on 30 frames to provide consistancy for physics calculation
+        * whether or not the frame cap is actually 30 frames.
+        * 
+        * There's probably a much better way to handle this but I'll figure that out once I get to work on
+        * the Timer class I'm working on. In the meantime, this is more than enough for now.
+        * 
+        * There's also probably another term for what I'm doing right now, since delta time is the time
+        * between frames. This is obviously not the time between frames going on right now so its probably
+        * something else.
+        */
         //Update the game
-        app->Update(deltaTime);
+        app->Update(((1000 / 30) - frameTime) / 1000.0f);
 
         //Calculate Delta Time for framerate
         frameTime = SDL_GetTicks() - lastTicks;
-        if ((1000 / app->fps) > frameTime)
-            SDL_Delay((1000 / app->fps) - frameTime);
+        if (app->isCapped)
+            if ((1000 / app->fps) > frameTime)
+            {
+                SDL_Delay((1000 / app->fps) - frameTime);
+            }
     }
 
     app->~Application();
