fd494dc56a3f0805add3b8bf483b73ff41e3aea9
Woodensponge <43834352+Woodensponge@users.noreply.github.com>
2020-12-29 12:47:38 -0500

    Begin working on crude tileset system

--- Hare/Level.cpp
+++ Hare/Level.cpp
@@ -3,6 +3,25 @@
 
 //TODO: Create a crude tileset system in this CPP file, then create and actual one in the TileSet files
 
+int rawMap[15][15]
+{
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+	{ 1,1,1,0,0,0,0,0,0,0,0,0,1,1,1 },
+	{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 }
+};
+
 Level::Level(int width, int height)
 {
 	this->width = width;
@@ -13,6 +32,9 @@ Level::Level(SDL_Window* window)
 {
 	SDL_GetWindowSize(window, (int*)&width, (int*)&height);
 	std::cout << "WINDOW WIDTH: " << width << " WINDOW HEIGHT: " << height << std::endl;
+
+	//Crude tileset system
+
 }
 
 Level::~Level()
